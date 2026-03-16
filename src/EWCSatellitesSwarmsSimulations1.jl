b00a94027694ec01a644f9a22b1fce78d5a6eb6f
Nuno Edgar Nunes Fernandes <nunoedgar.engineering@gmail.com>
2026-03-16 12:44:44 +0000

    Update EWCSatellitesSwarmsSimulations1.jl

--- src/EWCSatellitesSwarmsSimulations1.jl
+++ src/EWCSatellitesSwarmsSimulations1.jl
@@ -1,1 +1,9 @@
+module EWCSatellitesSwarmsSimulations1
 
+# Orbital helper functions - defined BEFORE generated code
+include("orbital_helpers.jl")
+
+# Dyad-generated code
+include("../generated/module.jl")
+
+end # module EWCSatellitesSwarmsSimulations1
